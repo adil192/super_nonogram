b9978aee1663e9541842c0faf4f842b9db2eafa7
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-06-21 01:51:31 +0100

    feat(title): add Play button

--- lib/components/pages/title_page.dart
+++ lib/components/pages/title_page.dart
@@ -6,25 +6,53 @@ class TitlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
+    final mediaQuery = MediaQuery.of(context);
+
+    final double titleFontSize;
+    final double buttonFontSize;
+    switch (mediaQuery.size.width) {
+      case < 400:
+        titleFontSize = 24;
+        buttonFontSize = 16;
+        break;
+      case < 600:
+        titleFontSize = 36;
+        buttonFontSize = 24;
+        break;
+      default:
+        titleFontSize = 48;
+        buttonFontSize = 32;
+    }
+
     return Scaffold(
       body: ColoredBox(
         color: colorScheme.primary,
         child: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Text(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
                 "Super Nonogram",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: switch (constraints.maxWidth) {
-                    < 400 => 24,
-                    < 600 => 36,
-                    _ => 48,
-                  },
+                  fontSize: titleFontSize,
                   color: colorScheme.onPrimary,
                 ),
-              );
-            },
+              ),
+              const SizedBox(height: 64),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(buttonFontSize / 2),
+                  child: Text(
+                    "Play",
+                    style: TextStyle(
+                      fontSize: buttonFontSize,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
