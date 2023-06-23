8e04126af99dee4523fea14bd839583c1fa9061a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-06-23 05:20:02 +0100

    feat: use device's dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,17 +27,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final baseTheme = ThemeData(brightness: Brightness.dark);
-    final theme = baseTheme.copyWith(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      textTheme: GoogleFonts.righteousTextTheme(baseTheme.textTheme),
-      useMaterial3: true,
-    );
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        lightDynamic ??= ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        );
+        darkDynamic ??= ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        );
 
-    return MaterialApp.router(
-      title: 'Super Nonogram',
-      theme: theme,
-      routerConfig: _router,
+        return MaterialApp.router(
+          title: 'Super Nonogram',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightDynamic,
+            textTheme: GoogleFonts.righteousTextTheme(),
+            scaffoldBackgroundColor: lightDynamic.background,
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkDynamic,
+            textTheme: GoogleFonts.righteousTextTheme(),
+            scaffoldBackgroundColor: darkDynamic.background,
+          ),
+          routerConfig: _router,
+        );
+      },
     );
   }
 }
