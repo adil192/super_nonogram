a8c9b6712d931082e33db738347103b19d108fad
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-07-10 10:54:00 +0100

    fix: center settings items on desktop

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -22,62 +22,64 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: Text(t.settings.settings),
       ),
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxWidth: 600,
-        ),
-        child: ListView(
-          children: [
-            // ad banner
-            if (AdState.adsSupported) const Padding(
-              padding: EdgeInsets.all(32),
-              child: BannerAdWidget(
-                adSize: AdSize.mediumRectangle,
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 600,
+          ),
+          child: ListView(
+            children: [
+              // ad banner
+              if (AdState.adsSupported) const Padding(
+                padding: EdgeInsets.all(32),
+                child: BannerAdWidget(
+                  adSize: AdSize.mediumRectangle,
+                ),
               ),
-            ),
-
-            // ad consent
-            if (AdState.adsSupported) SettingsBlob(
-              onTap: AdState.showConsentForm,
-              children: [
-                const FaIcon(FontAwesomeIcons.rectangleAd),
-                Text(
-                  t.settings.configureAdsConsent,
-                  style: TextStyle(
-                    color: colorScheme.onBackground,
+      
+              // ad consent
+              if (AdState.adsSupported) SettingsBlob(
+                onTap: AdState.showConsentForm,
+                children: [
+                  const FaIcon(FontAwesomeIcons.rectangleAd),
+                  Text(
+                    t.settings.configureAdsConsent,
+                    style: TextStyle(
+                      color: colorScheme.onBackground,
+                    ),
                   ),
-                ),
-              ],
-            ),
-
-            // hyperlegible font
-            SettingsBlob(
-              onTap: () => setState(() {
-                Prefs.hyperlegibleFont.value = !Prefs.hyperlegibleFont.value;
-              }),
-              children: [
-                Text(
-                  'Aa',
-                  style: TextStyle(
-                    fontSize: iconTheme.size ?? 24,
-                    color: colorScheme.onBackground,
+                ],
+              ),
+      
+              // hyperlegible font
+              SettingsBlob(
+                onTap: () => setState(() {
+                  Prefs.hyperlegibleFont.value = !Prefs.hyperlegibleFont.value;
+                }),
+                children: [
+                  Text(
+                    'Aa',
+                    style: TextStyle(
+                      fontSize: iconTheme.size ?? 24,
+                      color: colorScheme.onBackground,
+                    ),
                   ),
-                ),
-                Text(
-                  t.settings.hyperlegibleFont,
-                  style: TextStyle(
-                    color: colorScheme.onBackground,
+                  Text(
+                    t.settings.hyperlegibleFont,
+                    style: TextStyle(
+                      color: colorScheme.onBackground,
+                    ),
                   ),
-                ),
-                Switch(
-                  value: Prefs.hyperlegibleFont.value,
-                  onChanged: (_) => setState(() {
-                    Prefs.hyperlegibleFont.value = !Prefs.hyperlegibleFont.value;
-                  }),
-                ),
-              ],
-            ),
-          ],
+                  Switch(
+                    value: Prefs.hyperlegibleFont.value,
+                    onChanged: (_) => setState(() {
+                      Prefs.hyperlegibleFont.value = !Prefs.hyperlegibleFont.value;
+                    }),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
