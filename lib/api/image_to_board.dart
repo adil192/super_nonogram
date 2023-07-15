e8f206496647fbd7881deba4527cd4d070563d4b
Adil Hanney <adilhanney@disroot.org>
2023-07-15 10:39:30 +0100

    fix(ImageToBoard): crop off first row and column

--- lib/api/image_to_board.dart
+++ lib/api/image_to_board.dart
@@ -7,32 +7,34 @@ import 'package:super_nonogram/board/tile_state.dart';
 abstract class ImageToBoard {
   static const minSelectedRatio = 0.2;
 
-  // todo: make this async
+  /// The first row and column of the resized image are ignored
+  /// since they're always empty.
   static Future<BoardState?> importFromBytes(Uint8List bytes, [int width = 10]) async {
     var cmd = img.Command()
       ..decodeImage(bytes)
-      ..copyResize(width: width);
+      ..copyResize(width: width + 1, interpolation: img.Interpolation.cubic);
     await cmd.executeThread();
 
     final image = cmd.outputImage!;
 
     final BoardState board = List.generate(
-      image.height,
+      image.height - 1,
       (_) => List.generate(
-        width,
+        image.width - 1,
         (_) => ValueNotifier(TileState.empty),
       ),
     );
-    
+
     int selectedTiles = 0;
     for (final pixel in image) {
+      if (pixel.x == 0 || pixel.y == 0) continue; // ignore first row and column (always empty)
       final selected = pixel.aNormalized > 0.5;
       if (!selected) continue;
-      board[pixel.y][pixel.x].value = TileState.selected;
+      board[pixel.y - 1][pixel.x - 1].value = TileState.selected;
       selectedTiles++;
     }
 
-    final totalTiles = width * image.height;
+    final totalTiles = (image.width - 1) * (image.height - 1);
     if (selectedTiles < totalTiles * minSelectedRatio) {
       if (kDebugMode) print('ImageToBoard: too few tiles selected, try another image');
       return null;
