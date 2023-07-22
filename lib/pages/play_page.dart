40e0dd4c23c132eff212948f026c19be75463cea
Adil Hanney <adilhanney@disroot.org>
2023-07-22 19:45:20 +0100

    fix: prevent column taking up full screen height

--- lib/pages/play_page.dart
+++ lib/pages/play_page.dart
@@ -86,6 +86,7 @@ class _PlayPageState extends State<PlayPage> {
             ),
           ),
           content: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               if (imageBytes != null) ...[
                 ConstrainedBox(
