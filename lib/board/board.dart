d7c9c9d057e1accc09a3b29457bc5676782cbdbf
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-10-01 17:13:16 +0100

    ref: separate BoardGrid widget

--- lib/board/board.dart
+++ lib/board/board.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:super_nonogram/board/board_grid.dart';
 import 'package:super_nonogram/board/board_labels.dart';
 import 'package:super_nonogram/board/tile.dart';
 import 'package:super_nonogram/board/tile_state.dart';
@@ -255,105 +256,15 @@ class BoardWidgetState extends State<Board> {
               child: Stack(
                 fit: StackFit.expand,
                 children: [
-                  AlignedGridView.count(
-                    itemCount: width * height + width + height + 1,
-                    physics: const NeverScrollableScrollPhysics(),
-                    crossAxisCount: width + 1,
-                    itemBuilder: (context, index) {
-                      final int x = index % (width + 1) - 1;
-                      final int y = index ~/ (width + 1) - 1;
-                      late final colorScheme = Theme.of(context).colorScheme;
-                      return switch ((x, y)) {
-                        (-1, -1) => const SizedBox(),
-                        (-1, _) => Align(
-                          alignment: Alignment.centerRight,
-                          child: ValueListenableBuilder(
-                            valueListenable: currentAnswers,
-                            builder: (context, _, child) {
-                              final status = BoardLabels.statusOfRow(y, answer, currentAnswers.value);
-                              return AnimatedOpacity(
-                                opacity: switch (status) {
-                                  BoardLabelStatus.correct => 0,
-                                  _ => 1,
-                                },
-                                duration: const Duration(milliseconds: 500),
-                                child: DefaultTextStyle.merge(
-                                  style: TextStyle(
-                                    fontSize: Board.tileSize * 0.2,
-                                    color: switch (status) {
-                                      BoardLabelStatus.correct => colorScheme.primary,
-                                      BoardLabelStatus.incorrect => colorScheme.error,
-                                      BoardLabelStatus.incomplete => colorScheme.onBackground,
-                                    },
-                                  ),
-                                  child: child!,
-                                ),
-                              );
-                            },
-                            child: Text(
-                              answer.labelRow(y),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                        (_, -1) => Align(
-                          alignment: Alignment.bottomCenter,
-                          child: ValueListenableBuilder(
-                            valueListenable: currentAnswers,
-                            builder: (context, _, child) {
-                              final status = BoardLabels.statusOfColumn(x, answer, currentAnswers.value);
-                              return AnimatedOpacity(
-                                opacity: switch (status) {
-                                  BoardLabelStatus.correct => 0,
-                                  _ => 1,
-                                },
-                                duration: const Duration(milliseconds: 500),
-                                child: DefaultTextStyle.merge(
-                                  style: TextStyle(
-                                    height: colLabelLineHeight,
-                                    fontSize: Board.tileSize * 0.2,
-                                    color: switch (status) {
-                                      BoardLabelStatus.correct => colorScheme.primary,
-                                      BoardLabelStatus.incorrect => colorScheme.error,
-                                      BoardLabelStatus.incomplete => colorScheme.onBackground,
-                                    },
-                                  ),
-                                  child: child!,
-                                ),
-                              );
-                            },
-                            child: SizedBox(
-                              height: columnLabelsHeight,
-                              child: Padding(
-                                padding: const EdgeInsets.only(bottom: Board.tileSpacing / 2),
-                                child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Text(
-                                    answer.labelColumn(x),
-                                    textAlign: TextAlign.center,
-                                    overflow: kDebugMode ? TextOverflow.clip : TextOverflow.fade,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        _ => ValueListenableBuilder(
-                          valueListenable: board[y][x],
-                          builder: (context, tileState, child) {
-                            return AspectRatio(
-                              aspectRatio: 1,
-                              child: Padding(
-                                padding: const EdgeInsets.all(Board.tileSpacing / 2),
-                                child: Tile(
-                                  tileState: tileState,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      };
-                    },
+                  BoardGrid(
+                    width: width,
+                    height: height,
+                    answer: answer,
+                    currentAnswers: currentAnswers,
+                    board: board,
+
+                    colLabelLineHeight: colLabelLineHeight,
+                    columnLabelsHeight: columnLabelsHeight,
                   ),
                   if (widget.srcImage != null) Opacity(
                     opacity: 0.2,
