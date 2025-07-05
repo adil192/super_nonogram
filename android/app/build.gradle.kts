558cc151cd83b5ba10a089aa8dd957d46eab5b3d
Adil Hanney <adilhanney@disroot.org>
2025-07-05 23:19:18 +0100

    fix: retire remove_signing_android patch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.adilhanney.super_nonogram"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
--- patches/remove_signing_android.sh
+++
@@ -1,5 +0,0 @@
-# This patch is necessary when building an appbundle on GitHub Actions,
-# as the signing action will fail if the .aab was already signed.
-
-# Remove `signingConfig = signingConfigs.getByName("release")` from android/app/build.gradle.kts
-sed -i -e 's/signingConfig = signingConfigs.getByName("release")//g' android/app/build.gradle.kts
